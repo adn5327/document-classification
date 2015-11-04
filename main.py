d70bff3099ef255c59d40a91978a054d1bdc198b
Jakub Klapacz <kubaklapacz@gmail.com>
2015-11-03 20:13:50 -0600

    Created main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+__author__ = 'Jakub Klapacz <jklapac2@illinois.edu> and Abhishek Nigam <adnigam2@illinois.edu>'
+
+
+'''
+	Main function
+'''
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
