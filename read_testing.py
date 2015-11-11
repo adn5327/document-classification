90c9a31a166fa1627eed3173bae5ffd4a299a5d3
Abhishek Deep Nigam <adn5327@gmail.com>
2015-11-11 17:00:10 -0600

    updated docs

--- read_testing.py
+++ read_testing.py
@@ -1,5 +1,7 @@
 import read_training as rtrain
 
+# this function doesn't actually care if bernoulli or not
+# that only matters for training
 def testdata(test_filename, train_filename, bernoulli):
 	train_data = rtrain.read_file(train_filename, bernoulli)
 
