56964c766ef16aae0b22271dfbd19bcfc4ca398d
Abhishek Deep Nigam <adn5327@gmail.com>
2015-11-11 16:59:23 -0600

    updated support for bernoulli or nah

--- read_testing.py
+++ read_testing.py
@@ -1,1 +1,8 @@
 import read_training as rtrain
+
+def testdata(test_filename, train_filename, bernoulli):
+	train_data = rtrain.read_file(train_filename, bernoulli)
+
+
+if __name__ == '__main__':
+	testdata('spam_detection/test_email.txt', 'spam_detection/train_email.txt', False)
